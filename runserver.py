b18b1a889f9e1939d178b7368d967315407eae9a
jadam1 <jadam1@kpmg.com.au>
2018-02-16 09:05:28 +1100

    Allowed flask to be serviced by apache

--- runserver.py
+++ runserver.py
@@ -3,6 +3,8 @@ from project import views
 from project import config
 
 # Return an App
+app = views.create_app(debug = config.DEBUG)
+
+#Run a localhost App
 if __name__ == "__main__":
-	app = views.create_app(debug = config.DEBUG)
 	app.run(host=config.HOST, port=config.PORT, threaded=True)
