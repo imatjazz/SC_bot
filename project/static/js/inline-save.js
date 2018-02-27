6a994292423a80b69751a0ab71d951215fc6ac97
Chantal <csuder1@kpmg.com.au>
2018-02-27 11:34:20 +1100

    inline edit working without field type or validation

---
+++ project/static/js/inline-save.js
@@ -0,0 +1,28 @@
+var timeoutId = {};
+
+function inlineSave () {
+    var id = $(this).parent()[0].id;
+    $('#status').addClass('label-danger').removeClass('label-success').html('changes pending <i class="fa fa-cog fa-spin"></i>');
+
+    // If a timer was already started, clear it.
+    if (timeoutId[id]) clearTimeout(timeoutId[id]);
+
+    // Set timer that will save comment when it fires.
+    timeoutId[id] = setTimeout(function () {
+        // Make ajax call to save data.
+        var data = {'field': id, 'fieldValue': $('td#' + id + ' input').val()};
+        var q = $.post(inlineSaveURL, data);
+        q.done(function(){
+        	$('#status').addClass('label-success').removeClass('label-danger').html('changes saved');
+        	console.log(data);
+        });
+        q.fail(function(err){
+        	alert('Something went wrong while saving your data: ' + JSON.stringify(err));
+        	$('#status').addClass('label-success').removeClass('label-danger').html('unsaved changes <i class="fa fa-exclamation-triangle"></i>');
+        	console.log(data);
+        });
+        
+    }, 1000);
+};
+
+$(document).on('input change keypress propertychange', '.editable input', inlineSave);
